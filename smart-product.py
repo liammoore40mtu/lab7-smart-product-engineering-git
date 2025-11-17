bf96a04859d291bc38602256b1ff728abad89d31
Liam Moore <ms0018628@mtu.ie>
2025-11-17 12:20:40 +0000

    This is the first commit

---
+++ smart-product.py
@@ -0,0 +1,8 @@
+print("This is Smart Product Introduction to Git Lab")
+
+#put your print name here
+
+print("My name is Liam Moore")
+
+#put your custom message here
+print("This is my custom message - LM")
