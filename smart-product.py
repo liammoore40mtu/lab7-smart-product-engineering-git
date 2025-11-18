651e14e17692bd083bc1379466fce9466774177e
sarahelcl <sarah.clancy@mycit.ie>
2025-11-18 17:06:37 +0000

    add my custom message to smart-product.py

--- smart-product.py
+++ smart-product.py
@@ -2,7 +2,6 @@ print("This is Smart Product Introduction to Git Lab")
 
 #put your print name here
 print ("My name is Sarah Clancy")
-print ("My name is Liam Moore")
 print ("This is a test")
 #put your custom message here
-print ("This is my custom message - LM")
+print ("Sarah's custom message -sc")
