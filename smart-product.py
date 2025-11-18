26153aab01efdf6292dfc199654217fdf1f3d654
IECSIM <liam@Liam-Linux.myguest.virtualbox.org>
2025-11-18 17:15:57 +0000

    Add my name to smart-prodcut.py

--- smart-product.py
+++ smart-product.py
@@ -2,7 +2,7 @@ print("This is Smart Product Introduction to Git Lab")
 
 #put your print name here
 
-print ("My name is Liam Moore")
+print ("My name is Bola")
 print ("This is a test")
 #put your custom message here
-print ("This is my custom message - LM")
+print ("This is my custom message - Bola")
